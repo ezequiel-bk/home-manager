52128ac132df933890c1b642138e030ddbba94f7
ezequiel-bk <programacionezequielk@gmail.com>
2024-12-19 06:58:16 -0300

    (feat) Added config for discord to prevent auto update - added obs config

--- programs/default.nix
+++ programs/default.nix
@@ -2,5 +2,7 @@
     imports = [
         ./kitty.nix
         ./tmux.nix
+        ./obs.nix
+        ./discord.nix
     ];
 }
---
+++ programs/discord.nix
@@ -0,0 +1,9 @@
+{  
+  home.file.".config/discord/settings.json" = {
+    text = ''
+    {
+      "SKIP_HOST_UPDATE": true
+    }
+    '';
+  };
+}
